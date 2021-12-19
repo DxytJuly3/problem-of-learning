4222618ec232e5a92bd973a25a451980f8b97ba8
July3 <dxyt2002@163.com>
2021-12-19 20:23:22 +0800

    Nowcoder_Update

---
+++ Oneself_Exercises/Nowcoder/BC95 最高分与最低分之差.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+//输入n个成绩，换行输出n个成绩中最高分数和最低分数的差。
+int main()
+{
+	int n = 0;
+	int max = 0;
+	int min = 10000;
+	int num = 0;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &num);
+		if (max < num)
+			max = num;
+		if (min > num)
+			min = num;
+	}
+	printf("%d", max - min);
+
+	return 0;
+}
+
