11e9ee1a1d8d30f6029b96813c2a0bd20f00099e
July3 <dxyt2002@163.com>
2021-12-27 21:53:38 +0800

    Nowcoder_Update

---
+++ Oneself_Exercises/Nowcoder/BC1 实践出真知.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+//于老师经常告诉我们“学习编程最好的办法就是上机实践，因为你要对计算机下指令，想让计算机帮你干活，就得多和计算机‘交流’，实践才能出真知。”
+int main()
+{
+    printf("Practice makes perfect!");
+    
+    return 0;
+}
---
+++ Oneself_Exercises/Nowcoder/BC10 成绩输入输出.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+//输入3科成绩，然后把三科成绩输出，成绩为整数形式。
+int main()
+{
+    int score1 = 0,score2 = 0,score3 = 0;
+    scanf("%d %d %d",&score1,&score2,&score3);
+    printf("score1=%d,score2=%d,score3=%d",score1,score2,score3);
+    
+    return 0;
+}
