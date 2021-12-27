1d46a044e03bb5b4791f649ba1017ab54f0ce1f9
July3 <dxyt2002@163.com>
2021-12-27 21:56:02 +0800

    Nowcoder_Update

---
+++ Oneself_Exercises/Nowcoder/BC6 小飞机.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//KiKi学会了printf在屏幕输出信息，他想输出一架小飞机。请帮他编写程序输出这架小飞机。
+int main()
+{
+    printf("     **\n");
+    printf("     **\n");
+    printf("************\n");
+    printf("************\n");
+    printf("    *  *\n");
+    printf("    *  *\n");
+    
+    return 0;
+}
