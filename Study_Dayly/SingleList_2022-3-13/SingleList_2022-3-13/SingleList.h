36e7d27b24598fa98a3651b368b68315fedf6e02
July3 <dxyt2002@163.com>
2022-03-19 18:12:44 +0800

    C语言实现数据结构 SingleList_2022-3-13(FINISH) UPDATE

--- Study_Dayly/SingleList_2022-3-13/SingleList_2022-3-13/SingleList.h
+++ Study_Dayly/SingleList_2022-3-13/SingleList_2022-3-13/SingleList.h
@@ -18,12 +18,29 @@ void SListPrint(SListNode *phead);
 // 链表尾插
 void SListPushBack(SListNode **pphead, SLTDateType x);
 
-void SListPopFront(SListNode **pphead);
-
+// 单链表头插
 void SListPushFront(SListNode **pphead, SLTDateType x);
 
-// 查找 x 所在位置 pos
-SListNode* SListFind(SListNode *phead, SListNode x);
+// 单链表头删
+void SListPopFront(SListNode **pphead);
 
-// 在pos位置插入
-void SListInsert(SListNode **pphead, SListNode *pos, SListNode x);
+// 单链表尾删
+void SListPopBack(SListNode **pphead);
+
+// 查找 x 所在位置 pos
+SListNode* SListFind(SListNode *phead, SLTDateType x);
+
+// 在pos位置之前插入
+void SListInsert(SListNode **pphead, SListNode *pos, SLTDateType x);
+
+// 在pos位置之后插入
+void SListInsertAfter(SListNode *pos, SLTDateType x);
+
+// 删除pos位置节点
+void SListErase(SListNode **pphead, SListNode *pos);
+
+// 删除pos位置之后节点
+void SListEraseAfter(SListNode *pos);
+
+// 销毁单链表
+void SListDestroy(SListNode **pphead);
