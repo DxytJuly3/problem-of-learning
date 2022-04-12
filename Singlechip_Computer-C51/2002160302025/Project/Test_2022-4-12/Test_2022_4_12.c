04716549d00feafb201d646e066552cbdedb4c87
July3 <dxyt2002@163.com>
2022-04-12 22:02:05 +0800

    UPDATE C51单片机 开关状态测试 2022-4-12

--- Singlechip_Computer-C51/2002160302025/Project/Test_2022-4-12/Test_2022_4_12.c
+++ Singlechip_Computer-C51/2002160302025/Project/Test_2022-4-12/Test_2022_4_12.c
@@ -3,23 +3,25 @@
 #include <REGX51.H>
 #include <intrins.h>
 
-void delay()
+void delay(int times)
 {
 	int i,j;
-	for(i = 0; i < 130; j++)
-		for(j = 0; j < 255; j++);	
-
+	while(times--)
+	{
+		for(i = 0; i < 130; i++)
+			for(j = 0; j < 255; j++);
+	}
 }
 
 void main()
 {
 	while(1)
 	{
-		unsigned char tmp;
-		P1 = 0xFF;
-		tmp = P1&0xF0;
-		tmp = tmp>>4;
-		P1 = tmp;
-		delay();
+		unsigned char temp;
+		P1=0xff;
+		temp=P1&0xf0;
+		temp=_cror_(temp, 4);
+		P1=temp;
+		delay(1);
 	}	
 }
