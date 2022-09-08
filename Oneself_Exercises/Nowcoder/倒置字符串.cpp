3640f57b0ef993a41ff16bc11a00e69c935f4651
July3 <dxyt2002@163.com>
2022-09-08 22:10:03 +0800

    添加 Nowcoder/倒置字符串.cpp 2022-9-8

---
+++ Oneself_Exercises/Nowcoder/倒置字符串.cpp
@@ -0,0 +1,36 @@
+/*
+【倒置字符串】将一句话的单词进行倒置，标点不倒置。比如 I like beijing. 经过函数后变为：beijing. like I
+输入描述：每个测试输入包含1个测试用例： I like beijing. 输入用例长度不超过100
+输出描述：依次输出倒置之后的字符串,以空格分割
+示例1：
+输入
+I like beijing.
+输出
+beijing. like I
+*/
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main() {
+    string str;
+    getline(cin, str);
+    reverse(str.begin(), str.end());
+    auto strBegin = str.begin();
+    auto strEnd = str.begin();
+    while(strEnd != str.end()) {
+        while(*strEnd != ' '&& strEnd != str.end()) {
+            strEnd++;
+        }
+        reverse(strBegin, strEnd);
+        while(*strEnd == ' ' && strEnd != str.end()) {
+            strEnd++;
+        }
+        strBegin = strEnd;
+    }
+
+    cout << str;
+    
+    return 0;
+}
