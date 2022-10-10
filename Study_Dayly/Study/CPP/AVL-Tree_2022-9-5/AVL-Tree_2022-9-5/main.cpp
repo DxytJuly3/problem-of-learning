e464c878911b08256847696f9dc368af1739fb06
DxytJuly <dxyt2002@163.com>
2022-10-10 23:47:54 +0800

    更新 Study/CPP/AVL-Tree AVL树相关代码 2022-10-10

--- Study_Dayly/Study/CPP/AVL-Tree_2022-9-5/AVL-Tree_2022-9-5/main.cpp
+++ Study_Dayly/Study/CPP/AVL-Tree_2022-9-5/AVL-Tree_2022-9-5/main.cpp
@@ -1,6 +1,14 @@
 #include "AVL-Tree.h"
 
 int main() {
+	AVLTree<int> avl;
+	avl.insert(1);
+	avl.insert(2);
+	avl.insert(3);
+	avl.insert(4);
+	avl.insert(5);
+	avl.insert(6);
+	avl.insert(7);
 
 	return 0;
 }
