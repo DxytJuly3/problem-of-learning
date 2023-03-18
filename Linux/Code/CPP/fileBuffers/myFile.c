820bebeabf12497c52cd8fc678e303869062541b
DxytJuly <dxyt2002@163.com>
2023-03-18 12:16:37 +0800

    更新 Linux/Code/CPP/fileBuffers/ 提交更新了 学习理解文件缓冲区时的相关代码, 模拟实现了C文件操作的fopen、fclose、fflush、fwrite 2023-3-18

--- Linux/Code/CPP/fileBuffers/myFile.c
+++ Linux/Code/CPP/fileBuffers/myFile.c
@@ -6,10 +6,33 @@
 #include <string.h> 
 
 int main() {
-	printf("Hello world");
-	write(stdout->_fileno, "I am a process", strlen("I am a process"));
+	const char* str1 = "Hello printf\n";
+	const char* str2 = "Hello fprintf\n";
+	const char* str3 = "Hello fputs\n";
+	const char* str4 = "Hello write\n";
 
-	sleep(3);
+	// C库函数
+	printf("%s", str1);
+	fprintf(stdout, str2);
+	fputs(str3, stdout);
+
+	// 系统接口
+	write(stdout->_fileno, str4, strlen(str4));
+
+	fork();
+//	printf("Hello July");
+//	fprintf(stdout, "Hello July");
+//	fputs("Hello July", stdout);
+	// 如果没有关闭stdout, 这三个语句会在进程结束时正常输出
+
+	// 进程退出会自动刷新缓冲区
+	// 我们在这里将 stdout 关闭
+	// close(stdout->_fileno);
+
+//	printf("Hello world");
+//	write(stdout->_fileno, "I am a process", strlen("I am a process"));
+//
+//	sleep(3);
 
 	return 0;
 }
