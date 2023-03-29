290ce389125d5a59c8add9560cabcaa7ee6727ab
DxytJuly3 <dxyt2002@163.com>
2023-03-29 22:12:24 +0800

    更新 Linux/Code/CPP/procCommunicate 更新提交 更新共享内存的连接、删除、使用, 通过共享内存进程间通信等相关代码 2023-3-29

--- Linux/Code/CPP/procCommunicate/shareMem/common.hpp
+++ Linux/Code/CPP/procCommunicate/shareMem/common.hpp
@@ -1,3 +1,8 @@
 #include <iostream>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <unistd.h>
+
+#define SHM_SIZE 4096
+#define PATH_NAME ".ipcShm"
+#define PROJ_ID 0x14
--- Linux/Code/CPP/procCommunicate/shareMem/ipcShmServer.cpp
+++ Linux/Code/CPP/procCommunicate/shareMem/ipcShmServer.cpp
@@ -1,17 +1,57 @@
+// ipcShmServer 服务端代码, 即 接收端
+// 需要创建、删除共享内存块
 #include "common.hpp"
 using std::cout;
 using std::endl;
 using std::cerr;
 
 int main() {
-    int key = ftok(".ipcShm", 0x14);
-    int shmId = shmget(key, 4096, IPC_CREAT | IPC_EXCL);
+    // 0. 创建共享内存块
+    int key = ftok(PATH_NAME, PROJ_ID);
+
+    cout << "Create share memory begin. " << endl;
+    sleep(2);
+    int shmId = shmget(key, SHM_SIZE, IPC_CREAT | IPC_EXCL | 0666);
     if(shmId == -1) {
         cerr << "shmget error" << endl;
         exit(1);
     }
-    
-    cout << "shmget success, key: " << key << " , shmId: " << shmId << endl;
+    cout << "Creat share memory success, key: " << key << " , shmId: " << shmId << endl;
+
+    // 1. 连接共享内存块
+    sleep(2);
+    char* str = (char*)shmat(shmId, nullptr, 0);
+    if(str == (void*)-1) {
+        cerr << "shmat error" << endl;
+        exit(2);
+    }
+    cout << "Attach share memory success. " << endl;
+
+    // 2. 使用共享内存块
+    int cnt = 0;
+    while(cnt++ < 30) {
+        cout << str << endl;
+        sleep(1);
+    }
+    cout << "\nThe server has finished using shared memory. " << endl;
+
+    sleep(1);
+    // 3. 分离共享内存块
+    int resDt = shmdt(str);
+    if(resDt == -1) {
+        cerr << "shmdt error" << endl;
+    }
+    cout << "Detach share memory success. \n" << endl;
+
+    sleep(5);
+
+    // 4. 删除共享内存块
+    int res = shmctl(shmId, IPC_RMID, nullptr);
+    if(res == -1) {
+        cerr << "shmget error" << endl;
+        exit(2);
+    }
+    cout << "Delete share memory success. " << endl;
 
     return 0;
 }
